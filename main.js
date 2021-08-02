1ba861b96f268b41e72e1bcb4a077206b54b7694
Hunter Carlson <carlson.hunter@gmail.com>
2021-08-01 18:02:47 -0700

    moon

--- main.js
+++ main.js
@@ -46,6 +46,25 @@ function addStar() {
 
 Array(200).fill().forEach(addStar);
 
+const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+scene.background = spaceTexture;
+
+const hunterTexture = new THREE.TextureLoader().load("hunter.jpg");
+
+const hunter = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: hunterTexture }));
+
+scene.add(hunter);
+
+const moonTexture = new THREE.TextureLoader().load("moon.jpg");
+const normalTexture = new THREE.TextureLoader().load("normal.jpg");
+
+const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(3, 32, 32),
+    new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: normalTexture })
+);
+
+scene.add(moon);
+
 function animate() {
     requestAnimationFrame(animate);
 
